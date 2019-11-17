683df6092a9ce5fb1e5093a2c97cbd4ba480f48c
Kovács András <kovacs.andras@nik.uni-obuda.hu>
2019-11-17 18:56:24 +0100

    Add project files.

---
+++ LinuxTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinuxTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
